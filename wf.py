01d072f51d793955c2910480a92b2166972882fa
Akiva Bloch <akiva@kubiya.ai>
2025-11-25 12:39:27 +0200

    add steps

--- wf.py
+++ wf.py
@@ -1,7 +1,12 @@
+from _io import open_code
 from kubiya import workflow
+
+pita = open("pita.py").read()
 
 wf = (
     workflow("data-pipeline")
     .description("ETL pipeline for customer data processing")
     .step("greet", "echo 'Hello from Kubiya!'")
+    .step("pita", callback=lambda s: s.python(pita).depends("greet"))
+    .step("echo", callback=lambda s: s.shell("echo 'Pita done'").depends("pita"))
 )
